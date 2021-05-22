e2443ab0c4ddcb1f15de839c10a55a9148ea9224
kebo <itachi971009@gmail.com>
2021-05-23 00:09:49 +0800

    [fea] add 70.爬楼梯

---
+++ leetcode/70.爬楼梯.py
@@ -0,0 +1,55 @@
+#
+# @lc app=leetcode.cn id=70 lang=python3
+#
+# [70] 爬楼梯
+#
+# https://leetcode-cn.com/problems/climbing-stairs/description/
+#
+# algorithms
+# Easy (51.98%)
+# Likes:    1660
+# Dislikes: 0
+# Total Accepted:    447.7K
+# Total Submissions: 861.4K
+# Testcase Example:  '2'
+#
+# 假设你正在爬楼梯。需要 n 阶你才能到达楼顶。
+# 
+# 每次你可以爬 1 或 2 个台阶。你有多少种不同的方法可以爬到楼顶呢？
+# 
+# 注意：给定 n 是一个正整数。
+# 
+# 示例 1：
+# 
+# 输入： 2
+# 输出： 2
+# 解释： 有两种方法可以爬到楼顶。
+# 1.  1 阶 + 1 阶
+# 2.  2 阶
+# 
+# 示例 2：
+# 
+# 输入： 3
+# 输出： 3
+# 解释： 有三种方法可以爬到楼顶。
+# 1.  1 阶 + 1 阶 + 1 阶
+# 2.  1 阶 + 2 阶
+# 3.  2 阶 + 1 阶
+# 
+# 
+#
+
+# @lc code=start
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        # 动态规划 dp[i] = dp[i-1] + dp[i-2]
+        if n <= 2:
+            return n
+
+        dp = [0 for i in range(n+1)]
+        dp[1] = 1
+        dp[2] = 2
+        for i in range(3, n+1):
+            dp[i] = dp[i-1] + dp[i-2]
+        return dp[n]
+# @lc code=end
