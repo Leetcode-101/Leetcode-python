430d63c4daa8c3ce007dc0d9e644c65798ea3fcb
kebo <itachi971009@gmail.com>
2021-05-31 23:54:31 +0800

    [fea] 合并两个有序数组

---
+++ leetcode/88.合并两个有序数组.py
@@ -0,0 +1,76 @@
+#
+# @lc app=leetcode.cn id=88 lang=python3
+#
+# [88] 合并两个有序数组
+#
+# https://leetcode-cn.com/problems/merge-sorted-array/description/
+#
+# algorithms
+# Easy (50.94%)
+# Likes:    960
+# Dislikes: 0
+# Total Accepted:    365.6K
+# Total Submissions: 717.7K
+# Testcase Example:  '[1,2,3,0,0,0]\n3\n[2,5,6]\n3'
+#
+# 给你两个有序整数数组 nums1 和 nums2，请你将 nums2 合并到 nums1 中，使 nums1 成为一个有序数组。
+# 
+# 初始化 nums1 和 nums2 的元素数量分别为 m 和 n 。你可以假设 nums1 的空间大小等于 m + n，这样它就有足够的空间保存来自
+# nums2 的元素。
+# 
+# 
+# 
+# 示例 1：
+# 
+# 
+# 输入：nums1 = [1,2,3,0,0,0], m = 3, nums2 = [2,5,6], n = 3
+# 输出：[1,2,2,3,5,6]
+# 
+# 
+# 示例 2：
+# 
+# 
+# 输入：nums1 = [1], m = 1, nums2 = [], n = 0
+# 输出：[1]
+# 
+# 
+# 
+# 
+# 提示：
+# 
+# 
+# nums1.length == m + n
+# nums2.length == n
+# 0 
+# 1 
+# -10^9 
+# 
+# 
+#
+
+# @lc code=start
+class Solution:
+    def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+        """
+        Do not return anything, modify nums1 in-place instead.
+        """
+        # 双指针
+        sorted = []
+        p1, p2 = 0, 0
+        while p1 < m or p2 <n:
+            if p1 == m:
+                sorted.append(nums2[p2])
+                p2 += 1
+            elif p2 == n:
+                sorted.append(nums1[p1])
+                p1 += 1
+            elif nums1[p1] < nums2[p2]:
+                sorted.append(nums1[p1])
+                p1 += 1
+            else:
+                sorted.append(nums2[p2])
+                p2 += 1
+        # nums1 = sorted
+        # 使用[:] 不会改变地址引用，id(nums1)不会变
+        nums1[:] = sorted
+# @lc code=end
